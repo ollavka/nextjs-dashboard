aa945c38b812e437636e0acdb4dc0cd974307d2e
Serhii Seriogin <serhii.seregin@perfsol.tech>
2023-11-20 16:59:09 +0200

    added alt prop to Image

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -18,7 +18,7 @@ export default async function InvoicesTable({
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
-            {invoices?.map((invoice) => (
+            {invoices?.map((invoice: any) => (
               <div
                 key={invoice.id}
                 className="mb-2 w-full rounded-md bg-white p-4"
@@ -31,6 +31,7 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
+                        alt='invoice'
                       />
                       <p>{invoice.name}</p>
                     </div>
